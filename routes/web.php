00baec651e74171c10f6ca74c44584510c85c71a
TranManhThang <tranmanhthang96@gmail.com>
2021-10-24 10:25:53 +0700

    web layout

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::domain(config('app.main_domain'))->name('web.')->group(function () {
     Route::get('', [\App\Http\Controllers\Web\HomeController::class, 'index'])->name('home');
+    Route::get('/contact-us.html', [\App\Http\Controllers\Web\ContactUsController::class, 'index'])->name('contact-us');
+    Route::get('/about-us.html', [\App\Http\Controllers\Web\AboutUsController::class, 'index'])->name('about-us');
+    Route::get('/cart-page.html', [\App\Http\Controllers\Web\CartController::class, 'index'])->name('cart');
+    Route::resource('/cart', CartController::class);
+    Route::get('/blog.html', [\App\Http\Controllers\Web\BlogController::class, 'index'])->name('blog.index');
+    Route::resource('/blog', BlogController::class);
+    Route::get('/products.html', [\App\Http\Controllers\Web\ProductController::class, 'index'])->name('products.index');
+    Route::resource('/products', ProductController::class);
 });
 
 Route::group(['prefix' => 'filemanager', 'middleware' => ['web', 'admin.auth']], function () {
