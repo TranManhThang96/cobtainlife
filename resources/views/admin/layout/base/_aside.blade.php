a304704139f1fe60819e9bd1b97ca9a85feebf40
TranManhThang <tranmanhthang96@gmail.com>
2021-11-13 17:06:57 +0700

    layout

--- resources/views/admin/layout/base/_aside.blade.php
+++ resources/views/admin/layout/base/_aside.blade.php
@@ -3,83 +3,104 @@
     <div class="scroll-sidebar">
         <!-- Sidebar navigation-->
         <nav class="sidebar-nav">
-            <ul id="sidebarnav" class="p-t-30">
-                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Dashboard</span></a></li>
-                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{route('admin.orders.index')}}" aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span class="hide-menu">Đơn hàng</span></a></li>
+            <ul id="sidebarnav">
+                <li class="sidebar-item"> 
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/" aria-expanded="false">
+                        <i class="fa fa-tachometer-alt"></i>
+                        <span class="hide-menu">Dashboard</span>
+                    </a>
+                </li>
+                <li class="sidebar-item"> 
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{route('admin.orders.index')}}" aria-expanded="false">
+                        <i class="fa fa-shopping-cart"></i>
+                        <span class="hide-menu">Đơn hàng</span>
+                    </a>
+                </li>
+                
                 <li class="sidebar-item"> 
                     <a class="sidebar-link has-arrow waves-effect waves-dark {{in_array(request()->route()->getName(), ['admin.products.index', 'admin.orders.index']) ? 'active' : ''}}" href="javascript:void(0)" aria-expanded="false" data-type="{{request()->route()->getName()}}">
-                        <i class="mdi mdi-receipt"></i>
+                        <i class="fa fa-folder-open"></i>
                         <span class="hide-menu">Danh Mục & Sản Phẩm</span>
                     </a>
                     <ul aria-expanded="false" class="collapse  first-level {{in_array(request()->route()->getName(), ['admin.products.index', 'admin.orders.index']) ? 'in' : ''}}">
                         <li class="sidebar-item"><a href="{{route('admin.categories.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-outline"></i><span
+                                    class="fa fa-folder-open"></i><span
                                     class="hide-menu">Danh mục</span></a></li>
                         <li class="sidebar-item"><a href="{{route('admin.products.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-plus"></i><span
+                                    class="fa fa-tree"></i><span
                                     class="hide-menu">Sản Phẩm </span></a></li>
                     </ul>
                 </li>
 
                 <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
-                    <i class="mdi mdi-receipt"></i>
-                    <span class="hide-menu">Quản lý khách hàng</span>
+                    <i class="fa fa-users"></i>
+                        <span class="hide-menu">Quản lý khách hàng</span>
                     </a>
                     <ul aria-expanded="false" class="collapse  first-level">
                         <li class="sidebar-item"><a href="{{route('admin.customers.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-outline"></i><span
+                                    class="fa fa-user"></i><span
                                     class="hide-menu">Khách hàng</span></a></li>
                         <li class="sidebar-item"><a href="#" class="sidebar-link"><i
-                                    class="mdi mdi-note-plus"></i><span
+                                    class="fa fa-user-circle"></i><span
                                     class="hide-menu">Khách hàng subscribe</span></a></li>
                     </ul>
                 </li>
 
-                <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu">Nội dung</span></a>
+                <li class="sidebar-item"> 
+                    <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
+                        <i class="fa fas fa-clone"></i>
+                        <span class="hide-menu">Nội dung</span>
+                    </a>
                     <ul aria-expanded="false" class="collapse  first-level">
                         <li class="sidebar-item"><a href="{{route('admin.banners.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-outline"></i><span
+                                    class="fa fa-image"></i><span
                                     class="hide-menu">Banner</span></a></li>
-                        <li class="sidebar-item"><a href="#" class="sidebar-link"><i
+                        <!-- <li class="sidebar-item"><a href="#" class="sidebar-link"><i
                                     class="mdi mdi-note-plus"></i><span
-                                    class="hide-menu">Quản lý trang</span></a></li>
+                                    class="hide-menu">Quản lý trang</span></a></li> -->
                         <li class="sidebar-item"><a href="{{route('admin.news.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-plus"></i><span
+                                    class="fa fa-file"></i><span
                                     class="hide-menu">Bài viết</span></a></li>
                     </ul>
                 </li>
 
+                <li class="sidebar-item"> 
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/" aria-expanded="false">
+                        <i class="fas fa-h-square"></i>
+                        <span class="hide-menu">Cấu hình cửa hàng</span>
+                    </a>
+                </li>
 
                 <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
-                    <i class="mdi mdi-receipt"></i>
+                    <i class="fa fas fa-cogs"></i>
                     <span class="hide-menu">Thiết lập hệ thống</span></a>
                     <ul aria-expanded="false" class="collapse  first-level">
                         <li class="sidebar-item"><a href="{{route('admin.order-status.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-outline"></i><span
+                                    class="fas fa-asterisk"></i><span
                                     class="hide-menu">Trạng thái đơn hàng</span></a></li>
                         <li class="sidebar-item"><a href="{{route('admin.shipping-status.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-plus"></i><span
+                                    class="fas fa-truck"></i><span
                                     class="hide-menu">Trạng thái vận chuyển</span></a></li>
                         <li class="sidebar-item"><a href="{{route('admin.payment-status.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-plus"></i><span
+                                    class="fas fa-recycle"></i><span
                                     class="hide-menu">Trạng thái thanh toán</span></a></li>
                         <li class="sidebar-item"><a href="{{route('admin.suppliers.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-plus"></i><span
+                                    class="fas fa-user-secret"></i><span
                                     class="hide-menu">Nhà cung cấp</span></a></li>
                         <li class="sidebar-item"><a href="{{route('admin.brands.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-plus"></i><span
+                                    class="fas fa-university"></i><span
                                     class="hide-menu">Nhãn hiệu</span></a></li>
                         <li class="sidebar-item"><a href="{{route('admin.weight-class.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-plus"></i><span
+                                    class="fas fa-balance-scale"></i><span
                                     class="hide-menu">Khối lượng</span></a></li>
                         <li class="sidebar-item"><a href="{{route('admin.length-class.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-plus"></i><span
+                                    class="fas fa-minus"></i><span
                                     class="hide-menu">Kích thước</span></a></li>
                         <li class="sidebar-item"><a href="{{route('admin.attribute-group.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-plus"></i><span
+                                    class="fas fa-bars"></i><span
                                     class="hide-menu">Nhóm thuộc tính</span></a></li>
                         <li class="sidebar-item"><a href="{{route('admin.tax.index')}}" class="sidebar-link"><i
-                                    class="mdi mdi-note-plus"></i><span
+                                    class="far fa-calendar-minus"></i><span
                                     class="hide-menu">Thuế</span></a></li>
 
                     </ul>
---
+++ resources/views/web/layout/full.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- set the encoding of your site -->
+	<meta charset="utf-8">
+	<!-- set the Compatible of your site -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="csrf-token" content="{{ csrf_token() }}"/>
+	<!-- set the page title -->
+	<title>Botanical - HTML5 Ecommerce Template</title>
+	<!-- include the site Google Fonts stylesheet -->
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700%7CRoboto:300,400,500,700,900&display=swap" rel="stylesheet">
+	<!-- include the site bootstrap stylesheet -->
+	<link href="{{asset('dist/css/bootstrap.css')}}" rel="stylesheet"/>
+	<!-- include the site fontawesome stylesheet -->
+	<link href="{{asset('dist/css/fontawesome.css')}}" rel="stylesheet"/>
+	<!-- include the site stylesheet -->
+	<link href="{{asset('dist/css/cobtainlife.css')}}" rel="stylesheet"/>
+	<!-- include theme plugins setting stylesheet -->
+	<link href="{{asset('dist/css/plugins.css')}}" rel="stylesheet"/>
+	<!-- include theme color setting stylesheet -->
+	<link href="{{asset('dist/css/color.css')}}" rel="stylesheet"/>
+	<!-- include theme responsive setting stylesheet -->
+	<link href="{{asset('dist/css/responsive.css')}}" rel="stylesheet"/>
+	<!-- toast -->
+	<link href="{{asset('assets/libs/toastr/build/toastr.min.css')}}" rel="stylesheet"/>
+	<link href="{{asset('css/web/app.css')}}" rel="stylesheet">
+	@yield('css')
+</head>
+<body>
+	<!-- pageWrapper -->
+	<div id="pageWrapper">
+        <!-- header -->
+        @include('web.layout.base._header')
+
+		<!-- main -->
+        
+		<main>
+			@yield('content')
+
+            <div class="container mb-lg-24 mb-md-16 mb-10">
+                <!-- subscribeSecBlock -->
+                <section class="subscribeSecBlock bgCover col-12 pt-lg-24 pb-lg-12 pt-md-16 pb-md-8 py-10" style="background-image: url({{asset('dist/images/1170x465.png')}})">
+                    <header class="col-12 mainHeader mb-9 text-center">
+                        <h1 class="headingIV playfair fwEblod mb-4">Subscribe Our Newsletter</h1>
+                        <span class="headerBorder d-block mb-5"><img src="{{asset('dist/images/hbdr.png')}}" alt="Header Border" class="img-fluid img-bdr"></span>
+                        <p class="mb-6">Enter Your email address to join our mailing list and keep yourself update</p>
+                    </header>
+                    <form class="emailForm1 mx-auto overflow-hidden d-flex flex-wrap">
+                        <input type="email" class="form-control px-4 border-0" placeholder="Enter your mail...">
+                        <button type="submit" class="btn btnTheme btnShop fwEbold text-white py-3 px-4 py-md-3 px-md-4">Shop Now <i class="fas fa-arrow-right ml-2"></i></button>
+                    </form>
+                </section>
+            </div>
+			
+			@include('web.layout.base._footer_holder')
+		</main>
+
+        <!-- footer -->
+        @include('web.layout.base._footer')
+	</div>
+	<!-- include jQuery library -->
+	<script src="{{asset('assets/libs/jquery/dist/jquery.min.js')}}"></script>
+	<!-- include bootstrap popper JavaScript -->
+	<script src="{{asset('assets/libs/popper.js/dist/umd/popper.min.js')}}"></script>
+	<!-- include bootstrap JavaScript -->
+	<script src="{{asset('assets/libs/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+	<!-- include custom JavaScript -->
+	<script src="{{asset('dist/js/jqueryCustome.js')}}"></script>
+	<!-- toast -->
+	<script src="{{asset('assets/libs/toastr/build/toastr.min.js')}}"></script>
+	<script src="{{asset('js/web/app.js')}}"></script>
+	<script src="{{asset('js/web/common/local_storage.js')}}"></script>
+	<script src="{{asset('js/web/common/utils.js')}}"></script>
+	<script src="{{asset('js/web/common/cart.js')}}"></script>
+
+	@yield('script')
+</body>
+</html>
