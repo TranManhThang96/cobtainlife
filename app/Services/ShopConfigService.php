ad8f0e6dcaffb015f0583439af289efc14ce0f1c
TranManhThang <tranmanhthang96@gmail.com>
2021-11-27 11:42:45 +0700

    client say

--- app/Services/ShopConfigService.php
+++ app/Services/ShopConfigService.php
@@ -27,13 +27,35 @@ class ShopConfigService extends BaseService
         $code = $configs['code'];
         unset($configs['code']);
         unset($configs['_token']);
-        foreach($configs as $key=>$config) {
-            $data = [
-                'code' => $code,
-                'key' => $key,
-                'value' => $config
-            ];
-            $this->configRepository->updateOrCreate($data);
+        $keysExcept = ['client_say', 'client_name', 'client_job'];
+        foreach ($configs as $key => $config) {
+            if (!in_array($key, $keysExcept)) {
+                $data = [
+                    'code' => $code,
+                    'key' => $key,
+                    'value' => $config
+                ];
+                $this->configRepository->updateOrCreate($data);
+            }
+        }
+
+        if (isset($configs['client_say'])) {
+            $says = [];
+            foreach ($configs['client_say'] as $k => $say) {
+                $says[] = [
+                    'client_say' => $say,
+                    'client_name' => $configs['client_name'][$k] ?? '',
+                    'client_job' => $configs['client_job'][$k] ?? '',
+                ];
+            }
+            if (count($says) > 0) {
+                $data = [
+                    'code' => $code,
+                    'key' => 'client_says',
+                    'value' => json_encode($says)
+                ];
+                $this->configRepository->updateOrCreate($data);
+            }
         }
     }
 }
--- resources/views/web/layout/full.blade.php
+++ resources/views/web/layout/full.blade.php
@@ -7,6 +7,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="csrf-token" content="{{ csrf_token() }}"/>
+	<link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/images/favicon.png')}}">
 	<!-- set the page title -->
 	<title>Cobtainlife</title>
 	<!-- include the site Google Fonts stylesheet -->
